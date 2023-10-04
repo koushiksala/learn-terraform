4c6ee4f30eddebb74cdbbe1111fdd5ba32e0d3e7
koushik <koushik214@gmail.com>
2023-10-04 22:23:41 +0530

    learn terraform resource

--- 01-resources/main.tf
+++ 01-resources/main.tf
@@ -16,14 +16,7 @@ terraform {
   }
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+
 
 # Configure the AWS Provider
 provider "aws" {
