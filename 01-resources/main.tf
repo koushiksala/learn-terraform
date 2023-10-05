96f90487c6c817f0ae74b0eada733d2799210661
koushik <koushik214@gmail.com>
2023-10-05 23:29:00 +0530

    learn terraform resources

--- 01-resources/main.tf
+++ 01-resources/main.tf
@@ -7,23 +7,3 @@ resource "aws_instance" "web" {
   }
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.19.0"
-    }
-  }
-}
-
-
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-}
